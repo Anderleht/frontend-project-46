f84eba1f4325993e6dd4dbd39f737ed1be05ab92
Artem Donchenko <anderleht00712@gmail.com>
2022-12-14 20:41:59 +0300

    fix test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -12,9 +12,8 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 
-const path1 = '/Users/anderleht/Desktop/Обучение/frontend-project-46/__fixtures__/file1.json';
-const path2 = '/Users/anderleht/Desktop/Обучение/frontend-project-46/__fixtures__/file2.json';
-
+const path1 = getFixturePath('file1.json');
+const path2 = getFixturePath('file2.json');
 
 describe('genDiff', () => {
 	it('test 1', () => {
