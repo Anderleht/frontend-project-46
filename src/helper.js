bada4c02b1c21d470dc4ea36c1ea90f9737abb3b
Artem Donchenko <anderleht00712@gmail.com>
2022-12-22 18:09:53 +0300

    6 step

--- src/helper.js
+++ src/helper.js
@@ -8,37 +8,66 @@ const getAndSortKeys = (obj1, obj2) => {
 	return keys.sort();
 };
 
-const getDiff = (first, second) => {
-	const keys =  getAndSortKeys(first, second);
+const getDiff = (data1, data2) => {
+	const keys =  getAndSortKeys(data1, data2);
 	const result = keys.reduce((acc, key) => {
-		if (Object.hasOwn(first, key) && Object.hasOwn(second, key)) {
-			if (first[key] !== second[key]) {
-				acc[`- ${key}`] = first[key];
-				acc[`+ ${key}`] = second[key];
-			} else if (first[key] === second[key]) {
-				acc[`  ${key}`] = first[key];
-			}
-		} else if (Object.hasOwn(first, key) && !Object.hasOwn(second, key)) {
-			acc[`- ${key}`] = first[key];
-		} else if (!Object.hasOwn(first, key) && Object.hasOwn(second, key)) {
-			acc[`+ ${key}`] = second[key];
+		if (!Object.hasOwn(data1, key)) {
+			acc[key] = 'added';
+		} else if (!Object.hasOwn(data2, key)) {
+			acc[key] = 'deleted';
+		} else if (typeof data1[key] === 'object' && typeof data2[key] === 'object') {
+			acc[key] = 'nested';
+		} else if (data1[key] !== data2[key]) {
+			acc[key] = 'changed';
+		} else {
+			acc[key] = 'unchanged';
 		}
 		return acc;
 	}, {});
 	return result;
 };
-const objToString = (object) => {
-	const newArr = Object.entries(object);
-	const str = `{
-${newArr.join('\n')}
-}`;
-	const replacer = /,/g;
-	const fixedStr = str.replace(replacer, ': ');
-	return fixedStr;
+
+const stylish = (diffObj, data1, data2) => {
+	const diffKeys = _.keys(diffObj);
+	const result = diffKeys.sort().reduce((acc, key) => {
+		if (diffObj[key] === 'nested') {
+			acc[key] === getDiff(data1[key], data2[key]);
+		} else if (diffObj[key] === 'added') {
+			acc[`+ ${key}`] = data2[key];
+		} else if (diffObj[key] === 'deleted') {
+			acc[`- ${key}`] = data1[key];
+		} else if (diffObj[key] === 'changed') {
+			acc[`- ${key}`] = data1[key];
+			acc[`+ ${key}`] = data2[key];
+		} else if (diffObj[key] === 'unchanged') {
+			acc[` ${key}`] = data1[key];
+		}
+		return acc;
+	}, {});
+	return result;
+};
+
+
+const objToString = (value, replacer = ' ', spacesCount = 1) => {
+	const iter = (currentValue, depth) => {
+		if (typeof currentValue !== 'object' || currentValue === null) {
+			return String(currentValue);
+		}
+		const indentSize = spacesCount * depth;
+		const currentIndent = replacer.repeat(indentSize);
+		const bracketIndent = replacer.repeat(indentSize - spacesCount);
+  
+		const arrValue = Object.entries(currentValue);
+		const lines = arrValue.map(([key, val]) => `${currentIndent}${key}: ${iter(val, depth + 1)}`);
+		const result = ['{', ...lines, `${bracketIndent}}`].join('\n');
+  
+		return result;
+	};
+	return iter(value, 1);
 };
 
 const readFile = (filePath) => fs.readFileSync(path.resolve(filePath));
-const getParse = (filePath) => {
+const readAndParse = (filePath) => {
 	const format = path.extname(filePath);
 	const file = readFile(filePath);
 	let result;
@@ -52,8 +81,8 @@ const getParse = (filePath) => {
 };
  
 const genDiff = (filePath1, filePath2) => {
-	const firstObject = getParse(filePath1);
-	const secondObject = getParse(filePath2);
+	const firstObject = readAndParse(filePath1);
+	const secondObject = readAndParse(filePath2);
 	const resultObject = getDiff(firstObject, secondObject);
 	const str = objToString(resultObject);
 	console.log(str);
