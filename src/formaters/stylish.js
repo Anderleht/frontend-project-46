2008b6a77e63bb5c906884e87fbdd90fb740427c
Artem Donchenko <anderleht00712@gmail.com>
2023-01-07 17:20:31 +0300

    fix indents in stylish.js

--- src/formaters/stylish.js
+++ src/formaters/stylish.js
@@ -1,41 +1,42 @@
+const indent = (depth, spacesCount = 4) => ' '.repeat(depth * spacesCount - 2);
 
+const stringify = (value, depth = 1) => {
+	if (typeof value!== 'object' || value === null) {
+		return String(value);
+	}
+	const arrValue = Object.entries(value);
+	const lines = arrValue.map(([key, val]) => `${indent(depth)}  ${key}: ${stringify(val, depth + 1)}`);
+	const result = ['{', ...lines, `${indent(depth, 3)}}`].join('\n');
+  
+	return result;
+};
 
 const stylish = (value) => {
-	const replacer = ' ';
-	const spacesCount = 2;
 	const iter = (currentValue, depth) => {
 		if (typeof currentValue !== 'object' || currentValue === null) {
 			return String(currentValue);
 		}
-		const indentSize = spacesCount * depth;
-		const currentIndent = replacer.repeat(indentSize);
-		const bracketIndent = replacer.repeat(indentSize - spacesCount);
-  
 		const arrValue = Object.entries(currentValue);
 		const lines = arrValue.map(([key, val]) => {
 			let line;
 			if (val.status === 'added') {
-				line = `${currentIndent}  + ${key}: ${iter(val.value, depth + 1)}`;
+				line = `${indent(depth)}+ ${key}: ${stringify(val.value, depth + 1)}`;
 			}
 			else if (val.status === 'nested') {
-				line = `${currentIndent}  ${key}: ${iter(val.value, depth + 1)}`;
+				line = `${indent(depth)}  ${key}: ${iter(val.value, depth + 1)}`;
 			}
 			else if (val.status === 'deleted') {
-				line = `${currentIndent}  - ${key}: ${iter(val.value, depth + 1)}`;
+				line = `${indent(depth)}- ${key}: ${stringify(val.value, depth + 1)}`;
 			}
 			else if (val.status === 'unchanged') {
-				line = `${currentIndent}    ${key}: ${iter(val.value, depth + 1)}`;
+				line = `${indent(depth)}  ${key}: ${stringify(val.value, depth + 1)}`;
 			}
 			else if (val.status === 'changed') {
-				line = `${currentIndent}  - ${key}: ${iter(val.value, depth + 1)}\n${currentIndent}  + ${key}: ${iter(val.value2, depth + 1)}`; 
-			}
-			else {
-				line = `${currentIndent}      ${key}: ${iter(val, depth + 1)}`;
+				line = `${indent(depth)}- ${key}: ${stringify(val.value, depth + 1)}\n${indent(depth)}+ ${key}: ${stringify(val.value2, depth + 1)}`; 
 			}
 			return line;
 		});
-		const result = ['{', ...lines, `${bracketIndent}}`].join('\n');
-  
+		const result = ['{', ...lines, `${indent(depth, 3)}}`].join('\n');
 		return result;
 	};
 	return iter(value, 1);
